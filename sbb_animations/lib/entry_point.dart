d92ee00739b19ae18655b51791114cac4d40694c
collinsmakamu95 <collinsmakamu95@gmail.com>
2023-01-13 14:02:33 +0300

    second commit

---
+++ sbb_animations/lib/entry_point.dart
@@ -0,0 +1,11 @@
+ import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+ class EntryPoint extends StatelessWidget{
+  const EntryPoint({super.key});
+  
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+ }
